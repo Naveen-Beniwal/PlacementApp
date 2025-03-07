dd2f71aefe5e9b0de8fa759a82b5f7155a38fc01
Naveen Beniwal <naveenbeniwal00001@gmail.com>
2025-03-07 17:51:00 +0530

    l

--- backend/server.js
+++ backend/server.js
@@ -8,26 +8,14 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-const corsOptions = {
-  origin: "https://placement-portal-seven.vercel.app",
-  methods: "GET,POST,PUT,DELETE,OPTIONS",
-  allowedHeaders: "Content-Type,Authorization",
-  credentials: true,
-};
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", corsOptions.origin);
-  res.header("Access-Control-Allow-Methods", corsOptions.methods);
-  res.header("Access-Control-Allow-Headers", corsOptions.allowedHeaders);
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
-app.use(cors(corsOptions));
-
+app.use(
+  cors({
+    origin: "https://placement-portal-seven.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 // MongoDB Connection
